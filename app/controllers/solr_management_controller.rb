447dd91716e657b411a72175b9785ec9a82b45dc
Richard Peng <richard@richardpeng.com>
2018-05-25 15:01:04 -0400

    Fix progress display on Solr management page

--- app/controllers/solr_management_controller.rb
+++ app/controllers/solr_management_controller.rb
@@ -2,17 +2,18 @@ class SolrManagementController < ApplicationController
   def index
     solr_response = Faraday.get("#{ENV['SOLR_URL']}/dataimport?command=status")
     solr_data = JSON.parse(solr_response.body)
-    @completed = solr_data['statusMessages']['Total Rows Fetched'].to_f
     @status = solr_data['status']
 
     if @status != 'idle'
       feed_data = Faraday.get ENV['FEED_URL']
+      @completed = solr_data['statusMessages']['Total Rows Fetched'].to_f
       @total = Nokogiri::XML(feed_data.body).css('resumptionToken').first['completeListSize'].to_f
+      @progress = (@completed / @total * 100).round(0)
     else
-      @total = @completed
+      @completed = 0
+      @total = 0
+      @progress = 0
     end
-
-    @progress = (@completed / @total * 100).round(0)
   end
 
   def update

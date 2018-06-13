59f9cf86e86c28a52768ab3debc27574064216c3
Richard Peng <richard@richardpeng.com>
2018-06-13 15:27:37 -0400

    Show download link for file upload items; Hide image viewer for file uploads

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -36,6 +36,7 @@ class CatalogController < ApplicationController
 
     config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
     config.show.partials.insert(1, :openseadragon)
+    config.show.partials << :show_download
 
     ## blacklight-maps configuration default values
     config.view.maps.geojson_field = "geojson_ssim"

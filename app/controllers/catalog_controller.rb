6c04ace0916d6fc1af5c033edf6785b135a837b8
Richard Peng <richard@richardpeng.com>
2018-07-11 10:10:46 -0400

    Update description label

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -93,7 +93,7 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field 'spotlight_upload_description_tesim', unless: :is_gallery_view?
+    config.add_index_field 'spotlight_upload_description_tesim', label: 'Description', unless: :is_gallery_view?
     config.add_index_field 'note_tesim', label: 'Notes', unless: :is_gallery_view?
     config.add_index_field 'date_issued_dr', label: 'Date Issued'
     config.add_index_field 'date_created_dr', label: 'Date Created'

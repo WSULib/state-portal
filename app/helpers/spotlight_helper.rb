341003cb7a378c876ed10346a5e98ee9870ffb93
Richard Peng <richard@richardpeng.com>
2018-07-31 12:42:56 -0400

    Show global navbar; Hide exhibit navbar if current exhibit is default exhibit; Make global search bar wider Merge pull request #2 from pulibrary/update-readme
    Update README

--- app/helpers/spotlight_helper.rb
+++ app/helpers/spotlight_helper.rb
@@ -11,4 +11,8 @@ module SpotlightHelper
   def has_thumbnail?
     @document[Spotlight::Engine.config.thumbnail_field].present?
   end
+
+  def default_exhibit
+    Spotlight::Exhibit.find_by_slug('default')
+  end
 end

bfb22ee28ea9e19d2adc471483d8837efd7946e2
Richard Peng <richard@richardpeng.com>
2018-08-17 15:58:26 -0400

    Rename masthead migration

--- db/migrate/20180817191351_add_masthead_to_site.rb
+++ db/migrate/20180817191351_add_masthead_to_site.rb
@@ -1,4 +1,4 @@
-class AddCaptionToFeaturedImages < ActiveRecord::Migration[5.2]
+class AddMastheadToSite < ActiveRecord::Migration[5.2]
   def change
     add_column :spotlight_sites, :masthead_caption, :string
     add_column :spotlight_sites, :masthead_caption_url, :string
